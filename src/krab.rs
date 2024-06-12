95a119765d1b6a4f20b03a5632c0ce74fc8f37d9
drcheeseface <tharun1@hotmail.co.uk>
2024-06-13 00:08:20 +0100

    feat: grow older method

--- src/krab.rs
+++ src/krab.rs
@@ -28,39 +28,63 @@ impl Krab {
         }
     }
     pub fn grow_older(&mut self) {
-        self.age += 1
+        self.age += 1;
+        match self.age {
+            0..=5 => {
+                self.stage = String::from("egg");
+            }
+            6..=66 => {
+                self.stage = String::from("baby");
+            }
+            67..=1507 => {
+                self.stage = String::from("teen");
+            }
+            1508..=5828 => {
+                self.stage = String::from("adult");
+            }
+            _ => {
+                self.stage = String::from("elder");
+            }
+        }
     }
+    
+    pub fn feed(&mut self) {
+        todo!()
+    }
+
 
     //getters
     #[warn(dead_code)]
-    pub fn size(&self) -> &u8 {
-        &self.size
-    }
     pub fn name(&self) -> &String {
         &self.name
-    }
-    pub fn hunger(&self) -> &u8 {
-        &self.hunger
-    }
-    pub fn happiness(&self) -> &u8 {
-        &self.happiness
-    }
-    pub fn health(&self) -> &u8 {
-        &self.health
     }
     pub fn age(&self) -> &u64 {
         &self.age
     }
-    pub fn weight(&self) -> &u8 {
-        &self.weight
-    }
-    pub fn mood(&self) -> &String {
-        &self.mood
-    }
-    pub fn status(&self) -> &String {
-        &self.status
-    }
-    pub fn stage(&self) -> &String {
-        &self.stage
-    }
+
+    // pub fn size(&self) -> &u8 {
+    //     &self.size
+    // }
+    // pub fn hunger(&self) -> &u8 {
+    //     &self.hunger
+    // }
+    // pub fn happiness(&self) -> &u8 {
+    //     &self.happiness
+    // }
+    // pub fn health(&self) -> &u8 {
+    //     &self.health
+    // }
+    // pub fn weight(&self) -> &u8 {
+    //     &self.weight
+    // }
+    // pub fn mood(&self) -> &String {
+    //     &self.mood
+    // }
+    // pub fn status(&self) -> &String {
+    //     &self.status
+    // }
+    // pub fn stage(&self) -> &String {
+    //     &self.stage
+    // }
+    
 }
